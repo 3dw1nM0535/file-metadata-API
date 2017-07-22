3b650b7b8a07188bb6e1141345fca3333705cc61
Edwin Moses <workockmoses@gmail.com>
2017-07-22 19:15:27 +0300

    multer for file parse

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
 
@@ -10,7 +14,18 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
-})
+});
+
+app.post('/uploads', upload.single('uploadFile'), (req, res) => {
+    fs.unlink(req.file.path, (err) => {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log(req.file.filename + "is deleted!");
+        }
+        res.json({fileSize: req.file.size});
+    });
+});
 
 
 app.listen(port, '127.0.0.1', () => {
