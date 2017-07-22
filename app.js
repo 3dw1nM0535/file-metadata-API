3c2e6401ea8b989e2ee4fe35c1adef747d17057d
Edwin Moses <workockmoses@gmail.com>
2017-07-22 18:56:59 +0300

    setup route

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var port = process.env.PORT || 3000;
 //Static files Middleware
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
 
 app.listen(port, '127.0.0.1', () => {
     console.log("Server listening for connecions");
